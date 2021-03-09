bf9341eeb7c4da757b356ccc0b32d0267a8477b7
dzikimlecz <j.pul312@gmail.com>
2021-03-09 15:43:41 +0100

    initial commit

---
+++ src/main/kotlin/me/dzikimlecz/timetabledatabase/TimetabledatabaseApplication.kt
@@ -0,0 +1,11 @@
+package me.dzikimlecz.timetabledatabase
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TimetabledatabaseApplication
+
+fun main(args: Array<String>) {
+	runApplication<TimetabledatabaseApplication>(*args)
+}
