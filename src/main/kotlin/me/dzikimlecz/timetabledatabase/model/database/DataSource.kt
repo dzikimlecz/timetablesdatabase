6c21d849bdcc9868641a83386acce5119a8893cc
dzikimlecz <j.pul312@gmail.com>
2021-04-16 19:41:45 +0200

    added interface for data source handling HTTP requests

---
+++ src/main/kotlin/me/dzikimlecz/timetabledatabase/model/database/DataSource.kt
@@ -0,0 +1,13 @@
+package me.dzikimlecz.timetabledatabase.model.database
+
+interface DataSource<E> {
+    fun retrieve() : Collection<E>
+
+    fun retrieve(key: String): E
+
+    fun create(e: E): E
+
+    fun patch(e: E): E
+    fun delete(key: String): E
+
+}
