2227230030b5de66bb365c015c1c21159d8f1ea3
dzikimlecz <j.pul312@gmail.com>
2021-04-16 19:43:52 +0200

    added string conversion methods

--- src/main/kotlin/me/dzikimlecz/timetabledatabase/model/SettlingPeriod.kt
+++ src/main/kotlin/me/dzikimlecz/timetabledatabase/model/SettlingPeriod.kt
@@ -1,13 +1,27 @@
 package me.dzikimlecz.timetabledatabase.model
 
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter.ISO_LOCAL_DATE
 
 data class SettlingPeriod(
     val start: LocalDate,
     val end: LocalDate,
 ) {
-    init {
-        if (start >= end)
-            throw IllegalArgumentException("Start date: $start must be earlier than end date: $end")
+    init { require (start < end) { "Start date: $start must be earlier than the end date: $end" } }
+
+    override fun toString(): String = "{${start.format(ISO_LOCAL_DATE)}::${end.format(ISO_LOCAL_DATE)}}"
+
+    companion object {
+        private val pattern = Regex("\\{\\d{4}(-\\d{2}){2}::\\d{4}(-\\d{2}){2}}")
+
+        fun of(string: String): SettlingPeriod {
+            require(validate(string)) { "Invalid value: $string" }
+            val dates = string.trim().removePrefix("{").removeSuffix("}").split("::")
+            return SettlingPeriod(LocalDate.parse(dates[0]), LocalDate.parse(dates[1]),)
+        }
+
+        fun of(start: LocalDate, end: LocalDate) = SettlingPeriod(start, end)
+
+        fun validate(string: String) = pattern.matches(string.trim())
     }
 }
