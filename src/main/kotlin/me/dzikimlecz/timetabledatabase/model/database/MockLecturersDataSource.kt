8d96404c4f2c4cfb12f74d6cff83c2f1e46c410a
dzikimlecz <j.pul312@gmail.com>
2021-04-16 19:43:02 +0200

    Mocked proper LecturersDataSource

--- src/main/kotlin/me/dzikimlecz/timetabledatabase/model/database/MockLecturersDataSource.kt
+++ src/main/kotlin/me/dzikimlecz/timetabledatabase/model/database/MockLecturersDataSource.kt
@@ -6,8 +6,40 @@ import org.springframework.stereotype.Repository
 @Repository
 class MockLecturersDataSource : LecturersDataSource {
 
-    private val lecturers = mutableListOf(Lecturer("A", "A", emptyMap()))
+    private val lecturers = mutableListOf(
+        Lecturer("Marcin Najman", hoursWorkedStrings = emptyMap()),
+        Lecturer("Jan Paweł II", hoursWorkedStrings = emptyMap()),
+        Lecturer("Józef Stalin", hoursWorkedStrings = emptyMap()),
+        Lecturer("Jeff Bezos", hoursWorkedStrings = emptyMap()),
+        Lecturer("Twój Stary", hoursWorkedStrings = emptyMap()),
+    )
 
-    override fun retrieveLecturers(): Collection<Lecturer> = lecturers
+    override fun retrieve(): Collection<Lecturer> = lecturers
+    override fun create(e: Lecturer): Lecturer {
+        require (lecturers.none { it.code == e.code }) { "Lecturer of code ${e.code} already exists." }
+        lecturers.add(e)
+        return e
+    }
+
+    override fun patch(e: Lecturer): Lecturer {
+        val indexToPatch = lecturers.indexOfFirst { it.code == e.code }
+        if (indexToPatch == -1) throw NoSuchElementException("There is no lecturer of code: ${e.code}")
+        lecturers[indexToPatch] = lecturers[indexToPatch].derive(e.name) {
+            clear()
+            this += e.hoursWorked
+        }
+        return lecturers[indexToPatch]
+    }
+
+    override fun delete(key: String): Lecturer {
+        val removed = try {
+            lecturers.first { it.code == key }
+        } catch(e: NoSuchElementException) {
+            throw NoSuchElementException("There is no lecturer of code: $key")
+        }
+        lecturers.remove(removed)
+        return removed
+    }
+
 
 }
