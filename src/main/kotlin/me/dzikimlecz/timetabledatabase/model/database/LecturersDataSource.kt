0425959b5f9104974def6531198637760d7f71a7
dzikimlecz <j.pul312@gmail.com>
2021-04-16 19:45:44 +0200

    fixup! added interface for DataSource working on lecturers

--- src/main/kotlin/me/dzikimlecz/timetabledatabase/model/database/LecturersDataSource.kt
+++ src/main/kotlin/me/dzikimlecz/timetabledatabase/model/database/LecturersDataSource.kt
@@ -2,9 +2,16 @@ package me.dzikimlecz.timetabledatabase.model.database
 
 import me.dzikimlecz.timetabledatabase.model.Lecturer
 
-interface LecturersDataSource {
+interface LecturersDataSource : DataSource<Lecturer> {
 
-    fun retrieveLecturers() : Collection<Lecturer>
+    fun retrieve(predicate: (Lecturer) -> Boolean) = retrieve().filter(predicate)
 
-    fun retrieveLecturers(predicate: (Lecturer) -> Boolean) = retrieveLecturers().filter(predicate)
+    override fun retrieve(key: String): Lecturer {
+        if (key.count { it.isWhitespace() } > 2) try {
+            return retrieve().first { it.name.equals(key, ignoreCase = true) }
+        } catch(_: NoSuchElementException) {}
+        return retrieve().firstOrNull { it.code.equals(key, ignoreCase = true) }
+            ?: throw NoSuchElementException("Could not find lecturer $key")
+    }
+
 }
