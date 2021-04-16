6e3c78b6f3bdfc8f5834e25751bf0ff29a246906
dzikimlecz <j.pul312@gmail.com>
2021-04-16 19:44:21 +0200

    implemented all used HTTP requests

--- src/main/kotlin/me/dzikimlecz/timetabledatabase/service/LecturerService.kt
+++ src/main/kotlin/me/dzikimlecz/timetabledatabase/service/LecturerService.kt
@@ -1,9 +1,15 @@
 package me.dzikimlecz.timetabledatabase.service
 
+import me.dzikimlecz.timetabledatabase.model.Lecturer
 import me.dzikimlecz.timetabledatabase.model.database.LecturersDataSource
 import org.springframework.stereotype.Service
 
 @Service
 class LecturerService(private val dataSource: LecturersDataSource) {
-    fun getLecturers() = dataSource.retrieveLecturers()
+    fun getLecturers() = dataSource.retrieve()
+    fun getLecturer(key: String): Lecturer = dataSource.retrieve(key)
+    fun addLecturer(lecturer: Lecturer): Lecturer = dataSource.create(lecturer)
+
+    fun patchLecturer(lecturer: Lecturer) = dataSource.patch(lecturer)
+    fun deleteLecturer(key: String) = dataSource.delete(key)
 }
