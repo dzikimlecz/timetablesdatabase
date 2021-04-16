d07e4bd1bd926079bd275a7911cf4cb35b235fa9
dzikimlecz <j.pul312@gmail.com>
2021-04-16 19:42:10 +0200

    added class for working with lecturers

--- src/main/kotlin/me/dzikimlecz/timetabledatabase/model/Lecturer.kt
+++ src/main/kotlin/me/dzikimlecz/timetabledatabase/model/Lecturer.kt
@@ -1,20 +1,34 @@
 package me.dzikimlecz.timetabledatabase.model
 
-data class Lecturer(
+import com.fasterxml.jackson.annotation.JsonIgnore
+
+class Lecturer(
     val name: String,
-    val code: String = name.substring(0..1),
-    val hoursWorked: Map<SettlingPeriod, Int>,
+    val code: String =
+        name.split(Regex("\\s"))
+            .joinToString(separator = "") { it[0].toString().toUpperCase() },
+    val hoursWorkedStrings: Map<String, Int>,
 ) {
+
+    @JsonIgnore
+    val hoursWorked: Map<SettlingPeriod, Int> = hoursWorkedStrings.mapKeys { SettlingPeriod.of(it.key) }
+
+    @JsonIgnore
     val totalHoursWorked = hoursWorked.values.stream().mapToInt { it.toInt() }.sum()
 
     inline fun derive(
         name: String = this.name,
         code: String = this.code,
         instruction: MutableMap<SettlingPeriod, Int>.() -> Unit,
-    ): Lecturer {
-        val mutableCopy = HashMap<SettlingPeriod, Int>(hoursWorked)
-        mutableCopy.instruction()
-        return Lecturer(name, code, mutableCopy)
+    ) = Lecturer(name, code, HashMap(hoursWorked).apply(instruction).mapKeys { it.key.toString() })
+
+    companion object {
+        fun of(name: String,
+               code: String =
+                   name.split(Regex("\\s"))
+                       .joinToString(separator = "") { it[0].toString().toUpperCase() },
+               hoursWorked: Map<SettlingPeriod, Int>
+        ) = Lecturer(name, code, HashMap(hoursWorked).mapKeys { it.key.toString() })
     }
 
 
