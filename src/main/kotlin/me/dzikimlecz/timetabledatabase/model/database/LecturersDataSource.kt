6c0c7600fabf75af6a1fac4391f6fe4c9d49988f
dzikimlecz <j.pul312@gmail.com>
2021-05-13 11:46:05 +0200

    overridden retrieve method to add documentation

--- src/main/kotlin/me/dzikimlecz/timetabledatabase/model/database/LecturersDataSource.kt
+++ src/main/kotlin/me/dzikimlecz/timetabledatabase/model/database/LecturersDataSource.kt
@@ -4,6 +4,13 @@ import me.dzikimlecz.lecturers.Lecturer
 
 interface LecturersDataSource : DataSource<Lecturer> {
 
+    /**
+     * Should return lecturer of given [key]
+     *
+     * if it doesn't contains any whitespace treats it as the code of a lecturer, otherwise as a name
+     */
+    override fun retrieve(key: String): Lecturer
+
     override fun create(lecturer: Lecturer): Lecturer
 
     override fun patch(lecturer: Lecturer): Lecturer
