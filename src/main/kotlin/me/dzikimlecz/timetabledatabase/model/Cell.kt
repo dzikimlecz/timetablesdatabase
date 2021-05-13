ded4439783c5243d500f8de0b6ad2fef087e5850
dzikimlecz <j.pul312@gmail.com>
2021-05-13 11:44:59 +0200

    added iterator to iterate through the contents of the cell

--- src/main/kotlin/me/dzikimlecz/timetabledatabase/model/Cell.kt
+++ src/main/kotlin/me/dzikimlecz/timetabledatabase/model/Cell.kt
@@ -1,17 +1,39 @@
 package me.dzikimlecz.timetabledatabase.model
 
-
+import java.util.concurrent.atomic.AtomicInteger
 
 
 data class Cell(
     val content: List<String>,
     val divisionOrientation: DivisionDirection?,
-)
+): Iterable<String> {
+
+    operator fun get(i: Int): String =
+        if (i > 0 && divisionOrientation === null)
+            throw IndexOutOfBoundsException("Cell has only one content field. Attempted to access field: $i")
+        else content[i]
+
+
+    override fun iterator(): Iterator<String> = this.CellIterator()
+
+    private inner class CellIterator: Iterator<String> {
+        val index = AtomicInteger(-1)
+
+        override fun hasNext(): Boolean =
+            if (index.get() < 0) true
+            else divisionOrientation !== null && index.get() < 1
+
+        override fun next(): String =
+            if (hasNext()) this@Cell[index.incrementAndGet()]
+            else throw NoSuchElementException()
+    }
+}
 
 enum class DivisionDirection {
-    VERTICAL, HORIZONTAL, NONE;
+    VERTICAL, HORIZONTAL;
 
     companion object {
-        fun of(value: String?) = valueOf(value?.toUpperCase() ?: "NONE")
+        fun of(value: String?): DivisionDirection? =
+            if (value !== null) valueOf(value.toUpperCase()) else null
     }
 }
