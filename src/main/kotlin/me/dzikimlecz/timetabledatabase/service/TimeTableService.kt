059c1d20fd96d3ff7704eeb71fd6e003f4ed64ff
dzikimlecz <j.pul312@gmail.com>
2021-05-13 11:49:09 +0200

    added function to verify that all lecturers from the schedule are in the database

--- src/main/kotlin/me/dzikimlecz/timetabledatabase/service/TimeTableService.kt
+++ src/main/kotlin/me/dzikimlecz/timetabledatabase/service/TimeTableService.kt
@@ -1,25 +1,47 @@
 package me.dzikimlecz.timetabledatabase.service
 
+import me.dzikimlecz.timetabledatabase.model.Cell
 import me.dzikimlecz.timetabledatabase.model.TimeTable
+import me.dzikimlecz.timetabledatabase.model.database.LecturersDataSource
 import me.dzikimlecz.timetabledatabase.model.database.TimeTablesDataSource
 import org.springframework.stereotype.Service
 
 @Service
-class TimeTableService(private val dataSource: TimeTablesDataSource) {
+class TimeTableService(
+    private val tablesSource: TimeTablesDataSource,
+    private val lecturersSource: LecturersDataSource,
+    private val lecturerService: LecturerService,
+) {
 
     fun getTimeTables(): Collection<TimeTable> =
-        dataSource.retrieve()
+        tablesSource.retrieve()
 
     fun getTimeTable(key: String): TimeTable =
-        dataSource.retrieve(key)
+        tablesSource.retrieve(key.trim())
 
-    fun addTimeTable(table: TimeTable): TimeTable =
-        dataSource.create(table)
+    fun addTimeTable(table: TimeTable): TimeTable {
+        return tablesSource.create(table)
+    }
 
-    fun patchTimeTable(table: TimeTable): TimeTable =
-        dataSource.patch(table)
+    fun patchTimeTable(table: TimeTable): TimeTable {
+        return tablesSource.patch(table)
+    }
 
     fun deleteTimeTable(key: String): TimeTable =
-        dataSource.delete(key)
+        tablesSource.delete(key)
 
+    fun verifyAllLecturersFound(table: List<List<Cell>>) {
+        val notFoundCodes = mutableListOf<String>()
+        val foundCodes = mutableListOf<String>()
+        for (row in table) for (cell in row)
+            for (code in cell) if (code.isNotBlank() && code !in foundCodes) try {
+                foundCodes += lecturersSource.retrieve(code).code
+            } catch (e: NoSuchElementException) { notFoundCodes += code }
+        if (notFoundCodes.isNotEmpty())
+            throw LecturerNotFoundException(notFoundCodes)
+    }
+}
+
+class LecturerNotFoundException(val keys: List<String>): RuntimeException() {
+    init { require (keys.isNotEmpty()) { "Keys can't be empty" } }
 }
