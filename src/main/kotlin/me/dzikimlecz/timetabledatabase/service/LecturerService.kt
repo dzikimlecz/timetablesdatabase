e1276d53cbcac3fcd2147dd279d5cb43568da073
dzikimlecz <j.pul312@gmail.com>
2021-05-13 11:46:40 +0200

    added functions to update working times of lecturers

--- src/main/kotlin/me/dzikimlecz/timetabledatabase/service/LecturerService.kt
+++ src/main/kotlin/me/dzikimlecz/timetabledatabase/service/LecturerService.kt
@@ -1,6 +1,8 @@
 package me.dzikimlecz.timetabledatabase.service
 
 import me.dzikimlecz.lecturers.LecturerTransferredSurrogate
+import me.dzikimlecz.lecturers.SettlingPeriod
+import me.dzikimlecz.timetabledatabase.model.TimeTable
 import me.dzikimlecz.timetabledatabase.model.database.LecturersDataSource
 import org.springframework.stereotype.Service
 
@@ -15,4 +17,36 @@ class LecturerService(private val dataSource: LecturersDataSource) {
     fun patchLecturer(lecturer: LecturerTransferredSurrogate) = dataSource.patch(lecturer.toLecturer()).toSurrogate()
 
     fun deleteLecturer(key: String) = dataSource.delete(key).toSurrogate()
+
+    fun updateTimeWorked(table: TimeTable) {
+        val minutesWorked = collectTimeWorked(table)
+        val period: SettlingPeriod = table.period()
+        for ((code, minutes) in minutesWorked)
+            dataSource.retrieve(code).derive {
+                merge(period, minutes) { a, b -> a + b }
+            }.also { dataSource.patch(it) }
+    }
+
+    private fun collectTimeWorked(table: TimeTable): Map<String, Int> {
+        val durations = table.timeSpans.map { list -> list.map { it?.minutes?.toInt() ?: 0 } }
+        assert(durations.size == table.table.size)
+
+        val minutesWorked = mutableMapOf<String, Int>()
+        for (row in table.table) {
+            for ((i, pair) in row.withIndex()) {
+                val columnDurations = durations[i]
+                for ((j, code) in pair.withIndex()) if (code.isNotBlank())
+                    minutesWorked.merge(code, columnDurations[j]) { a, b -> a + b }
+            }
+        }
+
+        return minutesWorked
+    }
+}
+
+private fun TimeTable.period(): SettlingPeriod {
+    val start = this.date
+    val days = this.table.size - 1
+    val end = start.plusDays(days.toLong())
+    return SettlingPeriod(start, end)
 }
