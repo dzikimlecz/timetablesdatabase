defad59f1235315d1d1047cbd6685964d18a0786
dzikimlecz <j.pul312@gmail.com>
2021-06-04 18:51:00 +0200

    updated spring boot to 2.5.0; version 0.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.4.4"
+	id("org.springframework.boot") version "2.5.0"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.5.0"
 	kotlin("plugin.spring") version "1.5.0"
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "me.dzikimlecz"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
