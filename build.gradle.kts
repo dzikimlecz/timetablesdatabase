7407f4cc564d64b68bba3445b7ec9d5617661b7f
dzikimlecz <j.pul312@gmail.com>
2023-07-23 13:12:54 +0200

    updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,39 +1,46 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.5.0"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.5.0"
-	kotlin("plugin.spring") version "1.5.0"
-	kotlin("plugin.jpa") version "1.5.0"
+	id("org.springframework.boot") version "2.7.14"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+	kotlin("jvm") version "1.6.21"
+	kotlin("plugin.spring") version "1.6.21"
+	kotlin("plugin.jpa") version "1.6.21"
 }
 
 group = "me.dzikimlecz"
 version = "0.0.1"
-java.sourceCompatibility = JavaVersion.VERSION_11
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_11
+}
 
 repositories {
 	mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib")
-	implementation(files("C:\\libs\\transferred-1.jar"))
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.mockk:mockk:1.12.2")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	implementation(files("lib/timetables.transferred-1.0.1.jar"))
+	implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.21")
+
 	runtimeOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
-	runtimeOnly("com.h2database:h2")
+
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+	testImplementation("io.mockk:mockk:1.12.2")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
 }
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
+		freeCompilerArgs += "-Xjsr305=strict"
 		jvmTarget = "11"
 	}
 }
